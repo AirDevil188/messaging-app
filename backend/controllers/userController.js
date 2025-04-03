9d7bc62b392e65907000a3f733af551189fa0c0c
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2025-04-03 13:21:10 +0200

    add: userController with logInUser, createUser middlewares

---
+++ backend/controllers/userController.js
@@ -0,0 +1,68 @@
+const { body, validationResult } = require("express-validator");
+const asyncHandler = require("express-async-handler");
+const bcryptjs = require("bcryptjs");
+const db = require("../db/queries");
+const passport = require("passport");
+const generateToken = require("../config/jwt");
+
+const validateUser = [
+  body("username")
+    .trim()
+    .isLength({ min: 3 })
+    .withMessage("Username must contain at least 3 characters."),
+  body("password")
+    .isLength({ min: 8 })
+    .withMessage("Password must contain at least 8 characters."),
+  body("confirm_password")
+    .custom((value, { req }) => {
+      return value === req.body.password;
+    })
+    .withMessage("Password must match."),
+];
+
+const createUser = [
+  validateUser,
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(422).json([errors]);
+    }
+    const { username, password } = req.body;
+    bcryptjs.hash(password, 10, async (err, hashedPassword) => {
+      if (err) {
+        console.log(err);
+        return err;
+      }
+      const user = await db.createUser(username, hashedPassword);
+
+      return res.json(user);
+    });
+  }),
+];
+
+const logInUser = asyncHandler(async (req, res, next) => {
+  passport.authenticate(
+    "local",
+    { session: false },
+    async (err, user, info) => {
+      if (err) {
+        return next(err);
+      }
+      if (!user) {
+        return res.status(401).json({
+          message: "User not found.",
+        });
+      }
+      req.login(user, { session: false });
+      req.user = user;
+      const token = await generateToken.generateToken(req.user);
+      return res.json(token);
+    }
+  )(req, res, next);
+});
+
+module.exports = {
+  logInUser,
+  createUser,
+};
