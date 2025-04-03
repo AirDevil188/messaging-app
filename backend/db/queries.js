6c319452fff0c811f2f7ee9f1859be3c0dfbb999
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2025-04-03 13:17:49 +0200

    add: createUser, findUser, deserializeUser queries

---
+++ backend/db/queries.js
@@ -0,0 +1,47 @@
+const { PrismaClient } = require("@prisma/client");
+
+const prisma = new PrismaClient();
+
+async function findUser(username) {
+  try {
+    return await prisma.user.findFirst({
+      where: {
+        username: username,
+      },
+    });
+  } catch (err) {
+    console.log(err);
+    return err;
+  }
+}
+
+async function createUser(username, password) {
+  try {
+    await prisma.user.create({
+      data: {
+        username: username,
+        password: password,
+      },
+    });
+  } catch (err) {
+    console.log(err);
+    return err;
+  }
+}
+
+async function deserializeUser(id) {
+  try {
+    return prisma.user.findUnique({
+      where: id,
+    });
+  } catch (err) {
+    console.log(err);
+    return err;
+  }
+}
+
+module.exports = {
+  findUser,
+  createUser,
+  deserializeUser,
+};
