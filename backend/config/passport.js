d3dc08d477f318d347a233734eb433759d0737f9
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2025-04-22 12:22:59 +0200

    add: jwt and local strategy

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -2,31 +2,34 @@ const passport = require("passport");
 const bcrypt = require("bcryptjs");
 const LocalStrategy = require("passport-local").Strategy;
 const JwtStrategy = require("passport-jwt").Strategy;
+const ExtractJwt = require("passport-jwt").ExtractJwt;
 const db = require("../db/queries");
 const dotenv = require("dotenv");
 
 dotenv.config();
 
-passport.use(
-  new LocalStrategy(async (username, password, done) => {
-    try {
-      const user = await db.findUser(username);
+const passportLocalStrategy = () => {
+  passport.use(
+    new LocalStrategy(async (username, password, done) => {
+      try {
+        const user = await db.findUser(username);
 
-      if (!user) {
-        done(null, false, { message: "Incorrect Username!" });
-      }
-      const match = await bcrypt.compare(password, user.password);
+        if (!user) {
+          done(null, false, { message: "Incorrect Username!" });
+        }
+        const match = await bcrypt.compare(password, user.password);
 
-      if (!match) {
-        done(null, false, { message: "Incorrect Password!" });
+        if (!match) {
+          done(null, false, { message: "Incorrect Password!" });
+        }
+        return done(null, user);
+      } catch (err) {
+        console.log(err);
+        return err;
       }
-      return done(null, user);
-    } catch (err) {
-      console.log(err);
-      return err;
-    }
-  })
-);
+    })
+  );
+};
 
 passport.serializeUser((user, done) => {
   done(null, user.id);
@@ -41,16 +44,22 @@ passport.deserializeUser(async (id, done) => {
   }
 });
 
-passport.use(
-  new JwtStrategy(opts, async (jwt_payload, done) => {
-    const opts = {};
-    opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
-    opts.secretOrKey = process.env.SECRET;
-    try {
-      return done(null, { user: jwt_payload.user });
-    } catch (err) {
-      console.log(err);
-      return err;
-    }
-  })
-);
+const passportJWTStrategy = () => {
+  const opts = {};
+  opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+  opts.secretOrKey = process.env.SECRET;
+  passport.use(
+    new JwtStrategy(opts, async (jwt_payload, done) => {
+      console.log(process.env.SECRET);
+      try {
+        return done(null, { user: jwt_payload.user });
+      } catch (err) {
+        console.log(err);
+        return done(err);
+      }
+    })
+  );
+};
+
+passportLocalStrategy();
+passportJWTStrategy();
