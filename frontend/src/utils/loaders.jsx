6d9ba71c7c2686c597cdb63f97e26103a7d11a76
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2025-05-06 11:15:53 +0200

    add: loader util with getMessages

---
+++ frontend/src/utils/loaders.jsx
@@ -0,0 +1,19 @@
+import { handleFetch } from "./handleFetch";
+
+export const getMessages = async () => {
+  const res = await handleFetch("/messages", undefined, "GET");
+  console.log(res);
+  if (res.ok) {
+    return await res.json();
+  }
+
+  if (res.status === 401) {
+    const err = new Error("You are not authorized to access this page.");
+    return err;
+  }
+
+  if (res.status === 404) {
+    const err = new Error("There are no messages!");
+    return err;
+  }
+};
