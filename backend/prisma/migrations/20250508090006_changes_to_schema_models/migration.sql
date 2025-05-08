1d4891f9c965cfe1f490d39435c37afde5479473
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2025-05-08 11:00:52 +0200

    add: changes to db relations

---
+++ backend/prisma/migrations/20250508090006_changes_to_schema_models/migration.sql
@@ -0,0 +1,15 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[id]` on the table `User` will be added. If there are existing duplicate values, this will fail.
+  - Added the required column `sentMessagesId` to the `Messages` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "Messages" ADD COLUMN     "sentMessagesId" TEXT NOT NULL;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_id_key" ON "User"("id");
+
+-- AddForeignKey
+ALTER TABLE "Messages" ADD CONSTRAINT "Messages_sentMessagesId_fkey" FOREIGN KEY ("sentMessagesId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
