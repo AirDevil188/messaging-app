a9c7e96e0e676979d58b13dbb9311af74fdac135
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2025-05-12 11:37:43 +0200

    add: new user route

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -1,4 +1,5 @@
 const { Router } = require("express");
+const passport = require("passport");
 
 const userRouter = Router();
 const userController = require("../controllers/userController");
@@ -6,5 +7,11 @@ const userController = require("../controllers/userController");
 userRouter.post("/sign-up", userController.createUser);
 
 userRouter.post("/log-in", userController.logInUser);
+
+userRouter.get(
+  "/all-users",
+  passport.authenticate("jwt", { session: false }),
+  userController.getAllUsers
+);
 
 module.exports = userRouter;
