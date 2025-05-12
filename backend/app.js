2cff7ba84645a55a8a49363fb3593176d085aaa8
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2025-05-12 11:39:29 +0200

    change cors origin. add messageRouter

--- backend/app.js
+++ backend/app.js
@@ -5,13 +5,14 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const userRouter = require("./routes/userRouter");
+const messagesRouter = require("./routes/messageRouter");
 
 const app = express();
 require("./config/passport");
 
 app.use(
   cors({
-    origin: ["http://192.168.1.99:5173"],
+    origin: ["http://localhost:5173"],
     methods: "GET, PUT, POST, DELETE",
     optionsSuccessStatus: 204,
   })
@@ -21,6 +22,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/", userRouter);
+app.use("/messages", messagesRouter);
 
 app.listen(process.env.PORT, () => {
   console.log(`App is listening on the ${process.env.PORT}`);
