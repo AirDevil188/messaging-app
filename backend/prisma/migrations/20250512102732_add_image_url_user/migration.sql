187533b5ce1af3b12da771f8443a5ec4033c94a5
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2025-05-16 11:35:30 +0200

    add: migration imageUrl field to User model

---
+++ backend/prisma/migrations/20250512102732_add_image_url_user/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "imageUrl" TEXT NOT NULL DEFAULT 'https://res.cloudinary.com/ddxkbe6lh/image/upload/v1747045497/messages_app/wwartcllts0xmbvhp6zd.jpg';
