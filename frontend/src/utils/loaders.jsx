04dcc9aac7c1b31007d64c29f6975faa3903e90a
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2025-05-26 11:13:13 +0200

    add: getUsers loader function

--- frontend/src/utils/loaders.jsx
+++ frontend/src/utils/loaders.jsx
@@ -17,3 +17,20 @@ export const getMessages = async () => {
     return err;
   }
 };
+
+export const getUsers = async () => {
+  const res = await handleFetch("/all-users", undefined, "GET");
+  if (res.ok) {
+    return await res.json();
+  }
+
+  if (res.status === 401) {
+    const err = new Error("You are not authorized to access this page");
+    return err;
+  }
+
+  if (res.status === 404) {
+    const err = new Error("There are no users!");
+    return err;
+  }
+};
