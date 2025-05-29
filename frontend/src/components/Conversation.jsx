5420f9d49a331c6291171fa1aa8054f011eababf
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2025-05-29 13:39:45 +0200

    add: Conversation component and css module

---
+++ frontend/src/components/Conversation.jsx
@@ -0,0 +1,77 @@
+import { format, parseISO } from "date-fns";
+import styles from "./Conversation.module.css";
+import { useFetcher } from "react-router-dom";
+import Button from "./Button";
+import PropTypes from "prop-types";
+
+const Conversation = ({ user, conversation, setLoading, userObject }) => {
+  const fetcher = useFetcher();
+
+  console.log(userObject.user);
+  return (
+    <>
+      <section className={styles.conversationInfo}>
+        <section className={styles.conversationImage}>
+          <img src={user.imageUrl} alt="User avatar" />
+        </section>
+        <section className={styles.conversationUsername}>
+          <span>{user.username}</span>
+        </section>
+      </section>
+      <section className={`${styles.conversationContainer}`}>
+        {conversation?.messages ? (
+          <section className={styles.conversationWrapper}>
+            {conversation.messages.map((message) => {
+              return (
+                <article className={styles.message} key={message.id}>
+                  <section
+                    className={
+                      message.userId === userObject.user
+                        ? styles.senderInfo
+                        : styles.secondUserSenderInfo
+                    }
+                  >
+                    <span>{message.user.username}</span>
+                    <time>
+                      {format(parseISO(`${message.timestamp}`), "PPp")}
+                    </time>
+                  </section>
+
+                  <section
+                    className={
+                      message.userId === userObject.user
+                        ? styles.messageContent
+                        : styles.messageContentSecondUser
+                    }
+                  >
+                    <p>{message.text}</p>
+                  </section>
+                </article>
+              );
+            })}
+          </section>
+        ) : null}
+      </section>
+      <section className={styles.formContainer}>
+        <fetcher.Form method="POST">
+          <input type="hidden" name="userId" id="userId" value={user.id} />
+          <section className={styles.formGroup}>
+            <input type="text" name="text" id="text" required={true} />
+          </section>
+          <section className={styles.buttonContainer}>
+            <Button text={"SEND"} type={"submit"}></Button>
+          </section>
+        </fetcher.Form>
+      </section>
+    </>
+  );
+};
+
+Conversation.propTypes = {
+  user: PropTypes.object.isRequired,
+  conversation: PropTypes.object.isRequired,
+  userObject: PropTypes.object.isRequired,
+  setLoading: PropTypes.bool.isRequired,
+};
+
+export default Conversation;
