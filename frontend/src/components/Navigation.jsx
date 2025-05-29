790c5b680a98b4e392abe0886cf2fc8e990b7d7a
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2025-05-29 13:41:05 +0200

    add: Navigation component with css module

---
+++ frontend/src/components/Navigation.jsx
@@ -0,0 +1,34 @@
+import { FiMessageSquare, FiUsers, FiLogOut } from "react-icons/fi";
+import { RiGlobalLine } from "react-icons/ri";
+import styles from "./Navigation.module.css";
+
+const Navigation = () => {
+  return (
+    <header>
+      <nav>
+        <ul className={styles.sideBarMenu}>
+          <li className={styles.navButton}>
+            <a href="/messages">
+              <FiMessageSquare size={40} />
+            </a>
+          </li>
+          <li className={styles.navButton}>
+            <a>
+              <RiGlobalLine size={40} />
+            </a>
+          </li>
+          <li className={styles.navButton}>
+            <a href="/all-users">
+              <FiUsers size={40} />
+            </a>
+          </li>
+          <li className={styles.navButton}>
+            <FiLogOut size={40} />
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Navigation;
