853164aac4a266c1be3f6ab3eb37f457f0902de6
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2025-06-06 11:11:32 +0200

    add: messages route

--- backend/routes/messageRouter.js
+++ backend/routes/messageRouter.js
@@ -4,6 +4,12 @@ const passport = require("passport");
 const messageRouter = Router();
 const messageController = require("../controllers/messageController");
 
+messageRouter.get(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  messageController.getAllMessages
+);
+
 messageRouter.post(
   "/:id",
   passport.authenticate("jwt", { session: false }),
