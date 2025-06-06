88c19f8fb67be444386f8cddad7ef285785036ad
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2025-06-06 11:29:30 +0200

    fix: useEffect hoo add: Navigation component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,32 @@
 import { useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
 import { validateToken } from "./utils/validateToken";
+import Navigation from "./components/Navigation";
 
 function App() {
   const [userObject, setUserObject] = useState({ token: null });
 
   useEffect(() => {
     const token = validateToken();
+
     if (token) {
-      setUserObject({ ...userObject, token: token });
+      setUserObject({
+        ...userObject,
+        token: localStorage.getItem("token"),
+        user: token.user,
+      });
     }
   }, [userObject.token]);
 
   return (
-    <Outlet
-      context={{
-        userObject: [userObject, setUserObject],
-      }}
-    ></Outlet>
+    <>
+      <Navigation />
+      <Outlet
+        context={{
+          userObject: [userObject, setUserObject],
+        }}
+      ></Outlet>
+    </>
   );
 }
 
