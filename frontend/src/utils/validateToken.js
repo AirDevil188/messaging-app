7d7d5868bbcc0b63bb627bb6784355719ecf6568
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2025-06-06 11:35:01 +0200

    fix validateToken

--- frontend/src/utils/validateToken.js
+++ frontend/src/utils/validateToken.js
@@ -1,15 +1,14 @@
 import { jwtDecode } from "jwt-decode";
 
 export const validateToken = () => {
-  const token = localStorage.getItem("token");
-  if (token) {
-    const decodedToken = jwtDecode(token);
+  if (localStorage.getItem("token")) {
+    const decodedToken = jwtDecode(localStorage.getItem("token"));
     const currentDate = Date.now() / 1000;
 
     if (decodedToken.exp < currentDate) {
       localStorage.removeItem("token");
       return;
     }
-    return token;
+    return decodedToken;
   }
 };
