140103a6f6843256c2506eaa7663d8b21f5adfb8
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2025-06-06 11:06:47 +0200

    fix: delete console.log

--- backend/controllers/chatroomController.js
+++ backend/controllers/chatroomController.js
@@ -4,9 +4,7 @@ const db = require("../db/queries");
 const getChatroom = asyncHandler(async (req, res, next) => {
   const { user } = req.user;
   const user2 = req.params.id;
-  console.log(`user: ${user}`, `user2: ${user2}`);
   const conversation = await db.findChatroom(user, user2);
-  console.log(conversation);
   return res.json(conversation);
 });
 
