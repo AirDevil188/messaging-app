f1c5c7d48327b39658ad4e92ee11961d2574be3d
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2025-06-06 13:35:30 +0200

    add: getGlobalChatroom loader

--- frontend/src/utils/loaders.jsx
+++ frontend/src/utils/loaders.jsx
@@ -31,6 +31,20 @@ export const getUsers = async () => {
   }
 };
 
+export const getGlobalChatroom = async () => {
+  const res = await handleFetch("/global", undefined, "GET");
+  if (res.ok) {
+    return await res.json();
+  }
+  if (res.status === 401) {
+    throw new Error("You are not authorized to access this page!");
+  }
+
+  if (res.status === 401) {
+    throw new Error("Global chat Not Found!");
+  }
+};
+
 export const getAllChatRooms = async () => {
   const res = await handleFetch("/messages", undefined, "GET");
   if (res.ok) {
