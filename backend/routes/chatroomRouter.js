25d53c7275bb9a5167d58139e7c09704ff422a9c
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2025-06-10 10:12:11 +0200

    add: global GET and POST route

--- backend/routes/chatroomRouter.js
+++ backend/routes/chatroomRouter.js
@@ -6,9 +6,21 @@ const chatroomController = require("../controllers/chatroomController");
 const chatroomRouter = Router();
 
 chatroomRouter.get(
+  "/global",
+  passport.authenticate("jwt", { session: false }),
+  chatroomController.getGlobalChatroom
+);
+
+chatroomRouter.get(
   "/:id",
   passport.authenticate("jwt", { session: false }),
   chatroomController.getChatroom
+);
+
+chatroomRouter.post(
+  "/global",
+  passport.authenticate("jwt", { session: false }),
+  chatroomController.createMessageGlobalChatroom
 );
 
 module.exports = chatroomRouter;
