a57b5c25a1eb8d0c57feab8b45791fac4a3f3813
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2025-06-13 10:37:41 +0200

    add placeHolder prop and flex to formGroup class

--- frontend/src/components/FormWrapper.jsx
+++ frontend/src/components/FormWrapper.jsx
@@ -1,11 +1,24 @@
 import styles from "./FormWrapper.module.css";
 import PropTypes from "prop-types";
 
-const FormWrapper = ({ id, name, isRequired, labelText, inputType }) => {
+const FormWrapper = ({
+  id,
+  name,
+  isRequired,
+  labelText,
+  inputType,
+  placeholder,
+}) => {
   return (
     <section className={styles.formGroup}>
       <label htmlFor={name}>{labelText}</label>
-      <input id={id} name={name} required={isRequired} type={inputType}></input>
+      <input
+        id={id}
+        name={name}
+        required={isRequired}
+        type={inputType}
+        placeholder={placeholder}
+      ></input>
     </section>
   );
 };
@@ -16,6 +29,7 @@ FormWrapper.propTypes = {
   id: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
   isRequired: PropTypes.bool,
-  labelText: PropTypes.string.isRequired,
+  labelText: PropTypes.string,
+  placeholder: PropTypes.string,
   inputType: PropTypes.string.isRequired,
 };
