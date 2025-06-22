a099bb1a730223db14330e4ab758315cc73d0c26
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2025-06-22 16:46:29 +0200

    fix: getChatroom middleware

--- backend/controllers/chatroomController.js
+++ backend/controllers/chatroomController.js
@@ -2,9 +2,9 @@ const asyncHandler = require("express-async-handler");
 const db = require("../db/queries");
 
 const getChatroom = asyncHandler(async (req, res, next) => {
-  const { user } = req.user;
-  const user2 = req.params.id;
-  const conversation = await db.findChatroom(user, user2);
+  const chatroomId = req.params.id;
+  const conversation = await db.findChatroom(chatroomId);
+
   return res.json(conversation);
 });
 
