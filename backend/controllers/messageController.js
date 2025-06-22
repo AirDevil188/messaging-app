c552db3bdf64f4a1917815b88eed4a2e38d30469
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2025-06-22 16:47:31 +0200

    add: createMessageChatroom, createMessage middleware

--- backend/controllers/messageController.js
+++ backend/controllers/messageController.js
@@ -5,29 +5,46 @@ const db = require("../db/queries");
 const getAllMessages = asyncHandler(async (req, res, next) => {
   const { user } = req.user;
 
-  const [chatrooms] = await db.getAllChatRooms(user);
-  const { users } = chatrooms;
+  const chatrooms = await db.getAllChatRooms(user);
 
-  return res.json(users);
+  return res.json(chatrooms);
+});
+
+const getUsersMessages = asyncHandler(async (req, res, next) => {
+  const { user } = req.user;
+  const user2 = req.params.id;
+  const conversation = await db.findChatroomMessages(user, user2);
+  return res.json(conversation);
+});
+
+const createMessageChatroom = asyncHandler(async (req, res, next) => {
+  const { text, chatroomId, userId } = req.body;
+  const { user } = req.user;
+  const { id } = req.params;
+  console.log(user, userId);
+
+  const message = await db.createMessage(text, user, userId, chatroomId);
+  return res.json(message);
 });
 
 const createMessage = asyncHandler(async (req, res, next) => {
   const { text } = req.body;
   const { user } = req.user;
   const { id } = req.params;
-  const chatroom = await db.findChatroom(user, id);
+  const chatroom = await db.findChatroomMessages(user, id);
 
   if (chatroom) {
     const message = await db.createMessage(text, user, id, chatroom.id);
-
     return res.json(message);
   }
-  const message = await db.createMessage(text, user, id, "");
 
+  const message = await db.createMessage(text, user, id, "");
   return res.json(message);
 });
 
 module.exports = {
-  createMessage,
+  createMessageChatroom,
   getAllMessages,
+  getUsersMessages,
+  createMessage,
 };
