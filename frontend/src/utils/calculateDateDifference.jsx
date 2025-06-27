51cbbd058db7b02273e0362d0746f7fa495d4cc6
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2025-06-27 10:23:31 +0200

    add: month, year calc

--- frontend/src/utils/calculateDateDifference.jsx
+++ frontend/src/utils/calculateDateDifference.jsx
@@ -1,16 +1,32 @@
-import { format, differenceInCalendarDays } from "date-fns";
+import {
+  format,
+  differenceInCalendarDays,
+  differenceInMonths,
+  differenceInYears,
+} from "date-fns";
 
 export const calculateDates = (todayDate, msgDate) => {
-  const calculateDifference = differenceInCalendarDays(todayDate, msgDate);
-  switch (calculateDifference) {
-    case 0:
-      return format(msgDate, "HH:mm");
+  const calculateDifferenceDays = differenceInCalendarDays(todayDate, msgDate);
+  const calculateDIfferenceMonths = differenceInMonths(todayDate, msgDate);
+  const calculateDifferenceYears = differenceInYears(todayDate, msgDate);
 
-    case 1:
-      calculateDifference + " day ago";
-      break;
+  if (calculateDifferenceYears === 1) {
+    return calculateDifferenceYears + " year ago";
+  } else if (calculateDifferenceYears > 1) {
+    return calculateDifferenceYears + " years ago";
+  }
 
-    default:
-      return calculateDifference + " days ago";
+  if (calculateDIfferenceMonths === 1) {
+    return calculateDIfferenceMonths + " month ago";
+  } else if (calculateDIfferenceMonths > 1) {
+    return calculateDIfferenceMonths + " months ago";
+  }
+
+  if (calculateDifferenceDays === 1) {
+    return calculateDifferenceDays + " day ago";
+  } else if (calculateDifferenceDays > 1) {
+    return calculateDifferenceDays + " days ago";
+  } else {
+    return format(msgDate, "p");
   }
 };
