9b094280ff45f116224c4584992beb548b5a208c
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2025-06-27 11:23:11 +0200

    fix: unused imports. add: css styling

--- frontend/src/components/MessagesList.jsx
+++ frontend/src/components/MessagesList.jsx
@@ -1,18 +1,10 @@
-import Button from "./Button";
 import styles from "../components/MessagesList.module.css";
 import PropTypes from "prop-types";
-import { useLocation } from "react-router-dom";
-import { format, parseISO, differenceInCalendarDays, subDays } from "date-fns";
+
 import { calculateDates } from "../utils/calculateDateDifference";
 
 const startDate = new Date();
-
-const MessagesList = ({
-  messages,
-  handleUserMessages,
-  userObject,
-  previewMessages,
-}) => {
+const MessagesList = ({ messages, handleUserMessages, previewMessages }) => {
   return (
     <ul className={styles.userList}>
       {messages.map((chatroom) => {
@@ -28,30 +20,33 @@ const MessagesList = ({
               return (
                 <section key={user.id} className={styles.userWrapper}>
                   <section className={styles.userInfo}>
-                    <img src={user.imageUrl} alt="User avatar" />
-                    <span>{user.username}</span>
+                    <section className={styles.userImage}>
+                      <img src={user.imageUrl} alt="User avatar" />
+                    </section>
+                    <section className={styles.messageDetails}>
+                      <span>{user.username}</span>
+                      {previewMessages
+                        ? Object.values(previewMessages).map((msg) => {
+                            if (
+                              user.id === msg.userId ||
+                              user.id === msg.secondUserId
+                            ) {
+                              return (
+                                <section
+                                  className={styles.userMessage}
+                                  key={msg.id}
+                                >
+                                  <span>{msg.text + " "}</span>
+                                  <time>
+                                    {calculateDates(startDate, msg.timestamp)}
+                                  </time>
+                                </section>
+                              );
+                            }
+                          })
+                        : null}
+                    </section>
                   </section>
-
-                  {previewMessages
-                    ? Object.values(previewMessages).map((msg) => {
-                        if (
-                          user.id === msg.userId ||
-                          user.id === msg.secondUserId
-                        ) {
-                          return (
-                            <section
-                              className={styles.userMessage}
-                              key={msg.id}
-                            >
-                              <span>{msg.text + " "}</span>
-                              <time>
-                                {calculateDates(startDate, msg.timestamp)}
-                              </time>
-                            </section>
-                          );
-                        }
-                      })
-                    : null}
 
                   <section className={styles.userButtonSection}></section>
                 </section>
