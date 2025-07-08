2ea88a7d6cb7489ef73803bde2ca2ed72b92ece3
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2025-07-08 21:04:59 +0200

    add: globalChatroom component and css module

--- frontend/src/components/GlobalChatroom.jsx
+++ frontend/src/components/GlobalChatroom.jsx
@@ -1,21 +1,27 @@
-import { useLoaderData, useOutletContext } from "react-router-dom";
+import { useFetcher, useLoaderData, useOutletContext } from "react-router-dom";
 import styles from "./GlobalChatroom.module.css";
 import Conversation from "./Conversation";
 import { handleFetch } from "../utils/handleFetch";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import GlobalConversation from "./GlobalConversation";
 
 const GlobalChatroom = () => {
   const globalChatroom = useLoaderData();
-  const [conversation, setConversation] = useState({});
+
+  const [conversation, setConversation] = useState(
+    globalChatroom ? globalChatroom : null
+  );
+
   const {
     userObject: [userObject],
   } = useOutletContext();
 
   return (
-    <main>
-      <section className={styles.conversationSection}>
-        <Conversation
-          conversation={globalChatroom}
+    <main className={styles.mainGlobal}>
+      <section className={styles.globalConversationSection}>
+        <GlobalConversation
+          chatroom={globalChatroom}
+          conversation={conversation}
           userObject={userObject}
           setConversation={setConversation}
         />
@@ -32,7 +38,15 @@ export const handleGlobalMessageSubmit = async ({ request }) => {
     text: formData.text,
     chatroomId: formData.chatroomId,
   };
-  const res = await handleFetch(`/chatroom/global`, submission, "POST");
+  const res = await handleFetch(
+    `/chatroom/global`,
+    JSON.stringify(submission),
+    "POST",
+    {
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + localStorage.getItem("token"),
+    }
+  );
 
   return await res.json();
 };
