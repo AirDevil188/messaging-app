07d7f2cbb812fc879d2cde11380138fb41275263
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2025-07-10 20:20:57 +0200

    add: routerErrors and styling

--- frontend/src/components/ErrorElement.jsx
+++ frontend/src/components/ErrorElement.jsx
@@ -1,13 +1,37 @@
-import { useRouteError } from "react-router-dom";
+import { isRouteErrorResponse, useRouteError } from "react-router-dom";
 import styles from "./ErrorElement.module.css";
+import { TbFaceIdError } from "react-icons/tb";
 
 const ErrorElement = () => {
   const errors = useRouteError();
-
+  console.log(isRouteErrorResponse(errors));
   return (
-    <section className={styles.errorsSection}>
-      <p>{errors.message}</p>
-    </section>
+    <main className={styles.mainErrors}>
+      {isRouteErrorResponse(errors) ? (
+        <section className={styles.errorsSection}>
+          <section className={styles.errorsWrapper}>
+            <section className={styles.errorIcon}>
+              <TbFaceIdError size={300} />
+            </section>
+            <h1>
+              {errors.status} {errors.statusText}
+            </h1>
+            <section className={styles.buttonSection}>
+              <a href="/">
+                <button type="button">Go Back</button>
+              </a>
+            </section>
+          </section>
+        </section>
+      ) : (
+        <section className={styles.errorsSection}>
+          <p>{errors.message}</p>
+        </section>
+      )}
+    </main>
   );
+  // <section className={styles.errorsSection}>
+  //   <p>{errors.message}</p>
+  // </section>
 };
 export default ErrorElement;
